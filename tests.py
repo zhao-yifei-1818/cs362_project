051ae598b0b171938a3bbc8f87014fc91ced3d56
zhao-yifei-1818 <williamfei0000@gmail.com>
2026-05-30 22:03:12 -0700

    beautified, now align with flake8

--- tests.py
+++ tests.py
@@ -7,40 +7,41 @@ from task import _get_year_and_day_of_year
 from task import _get_month_and_day
 from task import _format_date
 from task import my_datetime
-class TestCase(unittest.TestCase):
 
+
+class TestCase(unittest.TestCase):
     def test_function1_valid(self):
-        self.assertEqual(conv_num('12345'),12345)
+        self.assertEqual(conv_num("12345"), 12345)
 
     def test_function1_valid_neg(self):
-        self.assertEqual(conv_num('-12345'),-12345)
-    
+        self.assertEqual(conv_num("-12345"), -12345)
+
     def test_function1_valid_neg2(self):
-        self.assertEqual(conv_num('-123.45'),-123.45)
+        self.assertEqual(conv_num("-123.45"), -123.45)
 
     def test_function1_valid_dec(self):
-        self.assertEqual(conv_num('.45'),.45)
+        self.assertEqual(conv_num(".45"), 0.45)
 
     def test_function1_valid_dec2(self):
-        self.assertEqual(conv_num('123.'),123.0)
+        self.assertEqual(conv_num("123."), 123.0)
 
     def test_function1_valid_hexadec(self):
-        self.assertEqual(conv_num('0xAD4'),2772)
+        self.assertEqual(conv_num("0xAD4"), 2772)
 
     def test_function1_valid_lowercase(self):
-        self.assertEqual(conv_num('0xad4'),2772)
+        self.assertEqual(conv_num("0xad4"), 2772)
 
     def test_function1_valid_neghexadec(self):
-        self.assertEqual(conv_num('-0xAD4'),-2772)
+        self.assertEqual(conv_num("-0xAD4"), -2772)
 
     def test_function1_invalid_hexa(self):
-        self.assertEqual(conv_num('0xAZ4'),None)
+        self.assertEqual(conv_num("0xAZ4"), None)
 
     def test_function1_invalid_str(self):
-        self.assertEqual(conv_num('12345A'),None)
-    
+        self.assertEqual(conv_num("12345A"), None)
+
     def test_function1_invalid_deci(self):
-        self.assertEqual(conv_num('12.3.45'),None)
+        self.assertEqual(conv_num("12.3.45"), None)
 
     def test_function2_invalid_leapyear_normal_year(self):
         self.assertEqual(_is_leap_year(2023), False)
@@ -59,7 +60,7 @@ class TestCase(unittest.TestCase):
 
     def test_function2_valid_seconds_to_days_one_day(self):
         self.assertEqual(_seconds_to_days(86400), 1)
-    
+
     def test_function2_valid_get_year_start(self):
         self.assertEqual(_get_year_and_day_of_year(0), (1970, 0))
 
@@ -89,27 +90,28 @@ class TestCase(unittest.TestCase):
 
     def test_function2_valid_my_datetime_example3(self):
         self.assertEqual(my_datetime(201653971200), "02-29-8360")
-        
+
     def test_function3_valid_big(self):
-        self.assertEqual(conv_endian(954786, 'big'),'0E 91 A2')
-    
+        self.assertEqual(conv_endian(954786, "big"), "0E 91 A2")
+
     def test_function3_valid_no_value(self):
-        self.assertEqual(conv_endian(954786),'0E 91 A2')
+        self.assertEqual(conv_endian(954786), "0E 91 A2")
 
     def test_function3_valid_neg(self):
-        self.assertEqual(conv_endian(-954786),'-0E 91 A2')
+        self.assertEqual(conv_endian(-954786), "-0E 91 A2")
 
     def test_function3_valid_little(self):
-        self.assertEqual(conv_endian(954786,'little'),'A2 91 0E')
-    
+        self.assertEqual(conv_endian(954786, "little"), "A2 91 0E")
+
     def test_function3_valid_little_neg(self):
-        self.assertEqual(conv_endian(-954786,'little'),'-A2 91 0E')
+        self.assertEqual(conv_endian(-954786, "little"), "-A2 91 0E")
 
     def test_function3_valid_declared(self):
-        self.assertEqual(conv_endian(num=-954786, endian='little'),'-A2 91 0E')
-    
-    def test_function3_invalid(self):
-        self.assertEqual(conv_endian(num=-954786, endian='small'),None)
+        self.assertEqual(conv_endian(num=-954786, endian="little"), "-A2 91 0E")
 
-if __name__ == '__main__':
+    def test_function3_invalid(self):
+        self.assertEqual(conv_endian(num=-954786, endian="small"), None)
+
+
+if __name__ == "__main__":
     unittest.main()
